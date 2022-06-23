f7947603816cee9e1d0e51f90352235ea7050fc9
unknown <khuillebagares@gmail.com>
2022-06-23 16:05:21 +0800

    Modify main file
    Added function to detect the image text

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-import pytesseract
+import pytesseract as pt
 import os
 import cv2
 from PIL import Image
 
+def showImage(image_path):
+    image = cv2.imread(image_path)
+    cv2.imshow("YOUR INPUT", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def testCode_listAllImageFiles():
+    for f in image_files:
+        print(f)
+
 test_img_path = "test images"
 create_path = lambda f: os.path.join(test_img_path, f)
 
-test_image_files = os.listdir(test_img_path)
+image_files = os.listdir(test_img_path)
 
-for f in test_image_files:
-    print(f)
+image_path = image_files[2]
+path = create_path(image_path)
 
+image = Image.open(path)
+text = pt.image_to_string(image)
 
+print(text)
+showImage(path)
